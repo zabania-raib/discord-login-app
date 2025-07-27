8652316bad55efa16823d12b3f6b05cba2e1d5a5
zabania-raib <zabania.raib@gmail.com>
2025-07-27 02:15:10 +0100

    chore: Force deploy with debug enabled

--- auth.ts
+++ auth.ts
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
-import { PrismaAdapter } from "@auth/prisma-adapter" // NEW LINE
-import { PrismaClient } from "@prisma/client"       // NEW LINE
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
-const client = new PrismaClient() // NEW LINE
+const client = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true, // <--- ADD THIS LINE
   trustHost: true,
-  adapter: PrismaAdapter(client), // <-- NEW LINE HERE
+  adapter: PrismaAdapter(client),
   providers: [
     Discord,
   ],
