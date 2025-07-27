d878fe4f95ab386e83f4f9391c5e3195fcda8722
zabania-raib <zabania.raib@gmail.com>
2025-07-27 01:41:10 +0100

    feat: Add database adapter

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
+import { PrismaAdapter } from "@auth/prisma-adapter" // NEW LINE
+import { PrismaClient } from "@prisma/client"       // NEW LINE
+
+const client = new PrismaClient() // NEW LINE
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   trustHost: true,
+  adapter: PrismaAdapter(client), // <-- NEW LINE HERE
   providers: [
     Discord,
   ],
